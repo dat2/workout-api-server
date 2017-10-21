31c28230f3d08440d0acc70509a273642f370b60
Nicholas Dujay <nickdujay@gmail.com>
2017-10-21 12:43:51 -0400

    add routes for workouts

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,49 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+extern crate chrono;
 extern crate rocket;
+extern crate rocket_contrib;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 
+use chrono::prelude::*;
+use rocket_contrib::Json;
 
-#[get("/")]
-fn index() -> &'static str {
-  "Hello, world!"
+// workout
+#[derive(Debug, Serialize, Deserialize)]
+struct Workout {
+  id: usize,
+  date_time: DateTime<Utc>
+}
+
+#[get("/workouts")]
+fn list_workouts() -> Json<Vec<Workout>> {
+  Json(Vec::new())
+}
+
+#[get("/workouts/<id>", format = "application/json")]
+fn get_workout(id: usize) -> Json<Workout> {
+  Json(Workout {
+    id: id,
+    date_time: Utc::now()
+  })
+}
+
+#[post("/workouts", format = "application/json", data = "<workout>")]
+fn create_workout(workout: Json<Workout>) -> () {
+  println!("{:?}", workout.id);
+  ()
 }
 
 fn main() {
-  rocket::ignite().mount("/", routes![index]).launch();
+  rocket::ignite()
+    .mount("/", routes![
+      list_workouts,
+      get_workout,
+      create_workout
+    ])
+    .launch();
 }
